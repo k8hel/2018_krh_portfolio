347fddf351819d88df88aeb334ed20c860171dad
[kaitlyn.helstrom] <[krhelstrom@hotmail.com]>
2018-05-03 23:18:09 -0600

    First commit

---
+++ src/colors.swift
@@ -0,0 +1,8 @@
+let primaryColor = UIColor(red: 0.98, green: 0.98, blue: 0.98, alpha: 1.0);
+let primaryLightColor = UIColor(red: 1.00, green: 1.00, blue: 1.00, alpha: 1.0);
+let primaryDarkColor = UIColor(red: 0.78, green: 0.78, blue: 0.78, alpha: 1.0);
+let primaryTextColor = UIColor(red: 0.10, green: 0.14, blue: 0.49, alpha: 1.0);
+let secondaryColor = UIColor(red: 0.05, green: 0.28, blue: 0.63, alpha: 1.0);
+let secondaryLightColor = UIColor(red: 0.33, green: 0.45, blue: 0.83, alpha: 1.0);
+let secondaryDarkColor = UIColor(red: 0.00, green: 0.13, blue: 0.44, alpha: 1.0);
+let secondaryTextColor = UIColor(red: 0.99, green: 0.85, blue: 0.21, alpha: 1.0);
